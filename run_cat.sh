95fe364c5baad818f888fd9b6ba4b199452e241c
Kunpeng Song <ks1418@degas.cs.rutgers.edu>
2023-01-14 05:12:58 -0500

    release_v1

--- run_cat.sh
+++ run_cat.sh
@@ -4,61 +4,61 @@ img_size=512
 
 prompt='photo of a dog'
 
-# exp_name="cat_2dog_Diffusion_minStep10_maxStep300_NoReg"
-# CUDA_VISIBLE_DEVICES=0 python train.py --size ${img_size} \
-# --batch 1 \
-# --n_sample 4 \
-# --output_dir logs/${exp_name} \
-# --lr 0.002  \
-# --frozen_gen_ckpt ${ckpt}  \
-# --iter 2001  \
-# --target_class "${prompt}"  \
-# --output_interval 50  \
-# --save_interval 10000000 \
-# --diffusion \
-# --freeze_bias \
-# --CFG 100 \
-# --min_step 10 \
-# --max_step 300 \
-# --num_inference_steps 50 \
-# --Disable_lpip \
+exp_name="cat_2dog_Diffusion_minStep10_maxStep300_NoReg"
+CUDA_VISIBLE_DEVICES=1 python train.py --size ${img_size} \
+--batch 1 \
+--n_sample 4 \
+--output_dir logs/${exp_name} \
+--lr 0.002  \
+--frozen_gen_ckpt ${ckpt}  \
+--iter 2001  \
+--target_class "${prompt}"  \
+--output_interval 50  \
+--save_interval 10000000 \
+--diffusion \
+--freeze_bias \
+--CFG 100 \
+--min_step 10 \
+--max_step 300 \
+--num_inference_steps 50 \
+--Disable_lpip \
 
-# exp_name="cat_2dog_Diffusion_minStep10_maxStep300_LpipsReg_w03"
-# CUDA_VISIBLE_DEVICES=0 python train.py --size ${img_size} \
-# --batch 1 \
-# --n_sample 4 \
-# --output_dir logs/${exp_name} \
-# --lr 0.002  \
-# --frozen_gen_ckpt ${ckpt}  \
-# --iter 2001  \
-# --target_class "${prompt}"  \
-# --output_interval 50  \
-# --save_interval 10000000 \
-# --diffusion \
-# --freeze_bias \
-# --CFG 100 \
-# --min_step 10 \
-# --max_step 300 \
-# --num_inference_steps 50 \
-# --lpip_weight_multiplicity 0.3
+exp_name="cat_2dog_Diffusion_minStep10_maxStep300_LpipsReg_w03"
+CUDA_VISIBLE_DEVICES=1 python train.py --size ${img_size} \
+--batch 1 \
+--n_sample 4 \
+--output_dir logs/${exp_name} \
+--lr 0.002  \
+--frozen_gen_ckpt ${ckpt}  \
+--iter 2001  \
+--target_class "${prompt}"  \
+--output_interval 50  \
+--save_interval 10000000 \
+--diffusion \
+--freeze_bias \
+--CFG 100 \
+--min_step 10 \
+--max_step 300 \
+--num_inference_steps 50 \
+--lpip_weight_multiplicity 0.3
 
-# exp_name="cat_2dog_Diffusion_minStep10_maxStep300_ReconstructionReg_w05"
-# CUDA_VISIBLE_DEVICES=0 python train.py --size ${img_size} \
-# --batch 1 \
-# --n_sample 4 \
-# --output_dir logs/${exp_name} \
-# --lr 0.002  \
-# --frozen_gen_ckpt ${ckpt}  \
-# --iter 2001  \
-# --target_class "${prompt}"  \
-# --output_interval 50  \
-# --save_interval 10000000 \
-# --diffusion \
-# --freeze_bias \
-# --CFG 100 \
-# --min_step 10 \
-# --max_step 300 \
-# --num_inference_steps 50 \
-# --Disable_lpip \
-# --Directional_loss 2 \
-# --directional_loss_weight_multiplicity 0.5
+exp_name="cat_2dog_Diffusion_minStep10_maxStep300_ReconstructionReg_w05"
+CUDA_VISIBLE_DEVICES=1 python train.py --size ${img_size} \
+--batch 1 \
+--n_sample 4 \
+--output_dir logs/${exp_name} \
+--lr 0.002  \
+--frozen_gen_ckpt ${ckpt}  \
+--iter 2001  \
+--target_class "${prompt}"  \
+--output_interval 50  \
+--save_interval 10000000 \
+--diffusion \
+--freeze_bias \
+--CFG 100 \
+--min_step 10 \
+--max_step 300 \
+--num_inference_steps 50 \
+--Disable_lpip \
+--Directional_loss 2 \
+--directional_loss_weight_multiplicity 0.5
